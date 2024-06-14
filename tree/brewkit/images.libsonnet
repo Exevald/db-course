c40ae2fcd6549c55fe79f0ae84ec153be3e84f55
Konstantin Kalinin <exevald@yandex.ru>
2024-06-14 21:34:59 +0300

    Развернул сервис, имитирующий работу древа жизни. Описал его api. Затолкал в сервис brewkit

---
+++ tree/brewkit/images.libsonnet
@@ -0,0 +1,5 @@
+{
+    gobuilder: "golang:1.22.1",
+    golangcilint: "golangci/golangci-lint:v1.54.2",
+    oapicodegen: "ghcr.io/veresnikov/docker-oapi-codegen:v2.1.0"
+}
---
+++ tree/cmd/tree/config.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/kelseyhightower/envconfig"
+	"github.com/pkg/errors"
+
+	"trees/pkg/tree/common/mysql"
+)
+
+func parseEnv() (*config, error) {
+	conf := new(config)
+	if err := envconfig.Process(appName, conf); err != nil {
+		return nil, errors.Wrap(err, "failed to parse environment variables")
+	}
+	return conf, nil
+}
+
+type dbConfig struct {
+	Host               string `envconfig:"host"`
+	Name               string `envconfig:"name"`
+	User               string `envconfig:"user"`
+	Password           string `envconfig:"password"`
+	MaxConn            int    `envconfig:"max_conn" default:"0"`
+	ConnectionLifetime int    `envconfig:"conn_lifetime" default:"0"`
+}
+
+type serviceConfig struct {
+	ServeRESTAddress string `envconfig:"serve_rest_address" default:":8082"`
+}
+
+type config struct {
+	DB      dbConfig      `envconfig:"db"`
+	Service serviceConfig `envconfig:"service"`
+}
+
+func (c *config) dsn() mysql.DSN {
+	return mysql.DSN{
+		Host:     c.DB.Host,
+		Database: c.DB.Name,
+		User:     c.DB.User,
+		Password: c.DB.Password,
+	}
+}
